79f45fde7426d51a469a2a734ab0d3174a0a0854
MdAkbar05 <samratakbar667466@gmail.com>
2023-06-05 22:57:52 +0600

    updated

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <App />
       <Routes>
         <Route path="/ToptenViewer" element={<App />}></Route>
         <Route path="technology" element={<Technology />}></Route>
